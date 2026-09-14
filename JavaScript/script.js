c1925a5f0235b8b14946807c3f9d356862285a45
asiff85 <reazulislamasif@example.com>
2026-09-15 00:32:25 +0600

    updated test cases

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -14,33 +14,57 @@ const user = () => ({
 });
 user().sayName();
 
-const JsPractice=() => {
+const JsPractice = () => {
     let numbers = [1, 2, 3, 4, 5];
     let squaredNumbers = numbers.map(num => num * num);
     console.log(`Squared Numbers: ${squaredNumbers}`);
-    let testCases=[
-        {id: 1,
-        title: "Login with valid credentials",
-        status: "passed",
-        priority: "high"},
-        {id: 2,
-        title: "Login with invalid credentials",
-        status: "failed",
-        priority: "high"},
-        {id: 3,
-        title: "Password reset",
-        status: "passed",
-        priority: "medium"}
+
+    let testCases = [
+        {
+            id: 1,
+            title: "Login with valid credentials",
+            status: "passed",
+            priority: "high"
+        },
+        {
+            id: 2,
+            title: "Login with invalid credentials",
+            status: "failed",
+            priority: "high"
+        },
+        {
+            id: 3,
+            title: "Password reset",
+            status: "passed",
+            priority: "medium"
+        }
     ];
-    return {testCases};
-    
-}
- console.log("Test Cases:", JsPractice().testCases);
 
-const passedTestCases = JsPractice().testCases.filter(testCase => testCase.status === "passed");
-const failedTestCases = JsPractice().testCases.filter(testCase => testCase.status === "failed");
+    const addTestCase = (id, title, status, priority) => {
+        const newTestCase = { id, title, status, priority };
+        testCases.push(newTestCase);
+    };
 
-console.log("Filtered Test Cases (Passed):", passedTestCases);
-console.log("Filtered Test Cases (Failed):", failedTestCases);
-console.log("Count of Passed Test Cases:", passedTestCases.length);
-console.log("Count of Passed Test Cases:", failedTestCases.length);
+    return { testCases, addTestCase };
+};
+
+const practice = JsPractice();
+practice.addTestCase(4, "User registration", "passed", "medium");
+
+console.log("Test Cases:", practice.testCases);
+
+const passedTestCases = practice.testCases.filter(testCase => testCase.status === "passed");
+const failedTestCases = practice.testCases.filter(testCase => testCase.status === "failed");
+
+// console.log("Filtered Test Cases (Passed):", passedTestCases);
+// console.log("Filtered Test Cases (Failed):", failedTestCases);
+// console.log("Count of Passed Test Cases:", passedTestCases.length);
+// console.log("Count of Failed Test Cases:", failedTestCases.length);
+
+let updatedTestCases = practice.testCases.map(testCase => {
+    if (testCase.status === "failed") {
+        return { ...testCase, status: "passed" };
+    }
+    return testCase;
+});
+console.log("Updated Test Cases:", updatedTestCases);
